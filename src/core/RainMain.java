a9da216e3147301183fb058300c867ee8d4c7636
weeklycode <n/a>
2018-04-07 18:58:11 -0400

    pastel rainbows!

--- src/core/RainMain.java
+++ src/core/RainMain.java
@@ -1,12 +1,17 @@
 package core;
 
 import java.awt.image.BufferedImage;
+import java.util.Random;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 //Copyright 2018 Ian Abrams
 
 public class RainMain {
+
+	private static final int[] LOWER = {97,105,97};
+	private static final int[] UPPER = {255,255,255};
 
 	public static void main(String[] args){
 
@@ -21,14 +26,26 @@ public class RainMain {
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jf.setVisible(true);
 		BufferedImage bi;
-		double[] rgb = {255,0,0};
+		double[] rgb = {130,130,130};
+		int r = new Random().nextInt(3);
+		rgb[(new Random().nextInt(2) + r)%3] = new Random().nextInt(256);
 		int phase = 0;
 		double[] initrgb = rgb.clone();//equal to rgb, but not a pointer
-		int initphase = phase;//primitive data type; no need to clone
+		int initphase = phase;//primitive data type, no need to clone
 		double change;
 		double[] nextrgb = rgb.clone();
 		int nextphase = phase;
+
+		BufferedImage oim = null;
+		try{
+			oim = ImageIO.read(RainMain.class.getResourceAsStream("null.png"));
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+
+		BufferedImage im = oim == null? null : new BufferedImage(1,1,BufferedImage.TYPE_INT_RGB);
 		do{
+
 			bi = new BufferedImage(jf.getWidth(), jf.getHeight(), BufferedImage.TYPE_INT_RGB);//uses one byte per rgb data, 3 bytes per pixel
 			if (reps>1){
 				change = (255*5+255*6*(reps-1))/(bi.getWidth()+bi.getHeight()-2);
@@ -36,13 +53,31 @@ public class RainMain {
 				change = (255*5*reps)/(bi.getWidth()+bi.getHeight()-2);
 			}
 
+			if (oim != null){
+				if (im.getWidth() != bi.getWidth() || im.getHeight() != bi.getHeight()){
+					im = new BufferedImage(bi.getWidth(),bi.getHeight(),BufferedImage.TYPE_INT_RGB);
+					im.getGraphics().drawImage(oim.getScaledInstance(bi.getWidth(), bi.getHeight(), BufferedImage.SCALE_SMOOTH), 0, 0, null);
+				}
+			}
+
 			for (int y = 0; y<bi.getHeight(); y++){
 				for (int x = 0; x<bi.getWidth(); x++){
-					bi.setRGB(x, y, ((int)rgb[0]<<16) | ((int)rgb[1]<<8) | (int)rgb[2]);
+
+					int c = -1;
+
+					if (im != null){
+						int ic = im.getRGB(x, y);
+						c = ((int)(Math.min((ic>>16)&255, rgb[0]))<<16) | ((int)(Math.min((ic>>8)&255, rgb[1]))<<8) | (int)(Math.min(ic&255, rgb[2]));
+					}else{
+						c = (((int)(rgb[0])<<16) | ((int)(rgb[1])<<8) | (int)(rgb[2]));
+					}
+
+					bi.setRGB(x, y, c);
+
 					phase=next(phase,rgb,change);
 				}
 
-				initphase = next(initphase,initrgb,change);
+				initphase = next(initphase,initrgb,change*((double)y/bi.getHeight()));
 				phase=initphase;
 				rgb=initrgb.clone();
 			}
@@ -59,25 +94,24 @@ public class RainMain {
 		} while(speed>0);
 	}
 
-
 	private static int next(int phase, double[] rgb, double change){
 		int affect;
 		if (phase%2==0){//adding
 			affect = (phase/2+1)%3;
-			if (rgb[affect]>=255){
+			if (rgb[affect]>=UPPER[affect]){
 				next(++phase,rgb,change);
 				return phase;
 			}else{
-				rgb[affect]=Math.min(255, rgb[affect]+change);
+				rgb[affect]=Math.min(UPPER[affect], rgb[affect]+change);
 				return phase;
 			}
 		}else{//subbing
 			affect = (phase/2)%3;
-			if (rgb[affect]<=0){
+			if (rgb[affect]<=LOWER[affect]){
 				next(++phase,rgb,change);
 				return phase;
 			}else{
-				rgb[affect]=Math.max(0, rgb[affect]-change);
+				rgb[affect]=Math.max(LOWER[affect], rgb[affect]-change);
 				return phase;
 			}
 		}
